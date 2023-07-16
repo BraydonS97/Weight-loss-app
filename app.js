de717697606ab45e1dfab6414c8b1025904ca422
Braydon Schmidt <BraydonS97@gmail.com>
2023-07-16 08:16:13 -0400

    Starting foundation

---
+++ app.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+    const searchButton = document.querySelector(".search");
+    searchButton.addEventListener('click', search);
+
+    function search() {
+        const loc = document.querySelector(".search-bar input").value;
+
+
+
+
+
+    }
+})
